1290fb8d6d416ce09f683eb265aab828d93ee0bc
Rian <rianoliveiracontato@gmail.com>
2022-10-24 20:04:07 -0300

    add item e batalha

--- fupFinal.py
+++ fupFinal.py
@@ -9,11 +9,11 @@ def item():
     qualItem = random.randint(1,3)
     if temItem() == "S":
         if qualItem == 1:
-            print("Item na sala: Espada")
+            return "Espada"
         elif qualItem == 2:
-            print("Item na sala: Escudo")
+            return "Escudo"
         else:
-            print("Item na sala: Bota")
+            return "Bota"
     else:
         return "Nenhum"
 def temInimigo():
@@ -26,42 +26,114 @@ def inimigo():
     qualInimigo = random.randint(1,4)
     if temInimigo() == "S":
         if qualInimigo == 1:
-            print("Monstro na sala: Slime")
+            return "Slime"
         elif qualInimigo == 2:
-            print("Monstro na sala: Zumbi")
+            return "Zumbi"
         elif qualInimigo == 3:
-            print("Monstro na sala: Esqueleto")
+            return "Esqueleto"
         else:
-            print("Monstro na sala: Aranha")
+            return "Aranha"
     else:
-        print("Monstro na sala: Nenhum")
+        return "Nenhum"
+def aplicandoItem(player,item):
+    if item == "Espada":
+        player["forca"] += 1
+        return player["forca"]
+    elif item == "Escudo":
+        player["defesa"] += 1
+        return player["defesa"]
+    else:
+        player["fuga"] += 1
+        return player["fuga"]
+
+def batalha(player, monstro):
+    if monstro == "Slime":
+        slime ={
+            "nome": "slime",
+            "forca": 2,
+            "defesa": 1
+        }
+        while player["defesa"] >= 0 or slime["defesa"] >= 0:
+            player["defesa"] -= slime["forca"]
+            slime["defesa"] -= player["forca"]
+        if player["defesa"] <= 0:
+            return player["defesa"]
+        else:
+            return "gameOver"
+    elif monstro == "Zumbi":
+        zumbi ={
+            "nome": "Zumbi",
+            "forca": 1,
+            "defesa": 3
+        }
+        while player["defesa"] >= 0 or zumbi["defesa"] >= 0:
+            player["defesa"] -= zumbi["forca"]
+            zumbi["defesa"] -= player["forca"]
+        if player["defesa"] <= 0:
+            return player["defesa"]
+        else:
+            return "gameOver"
+    elif monstro == "Esqueleto":
+        esqueleto ={
+            "nome": "esqueleto",
+            "forca": 3,
+            "defesa": 2
+        }
+        while player["defesa"] >= 0 or esqueleto["defesa"] >= 0:
+            player["defesa"] -= esqueleto["forca"]
+            esqueleto["defesa"] -= player["forca"]
+        if player["defesa"] <= 0:
+            return player["defesa"]
+        else:
+            return "gameOver"
+    elif monstro == "Aranha":
+        aranha ={
+            "nome": "aranha",
+            "forca": 3,
+            "defesa": 3
+        }
+        while player["defesa"] >= 0 or aranha["defesa"] >= 0:
+            player["defesa"] -= aranha["forca"]
+            aranha["defesa"] -= player["forca"]
+        if player["defesa"] <= 0:
+            return player["defesa"]
+        else:
+            return "gameOver"
+        
+    
 def addStatus(player):
     print(f"Quanto de cada <STATUS> você quer dar para {player['nome']}?")
     player["forca"] = int(input("Quanto de Força ? \n"))
     while player["forca"] <= 0 or player["forca"] > 5:
         print("Força muito alta ou INVALIDA. Novo valor:")
         player["forca"] = int(input("Quanto de Força ? \n"))
-    player["vitalidade"] = int(input("Quanto de Vitalidade ? \n"))
-    while player["vitalidade"] <= 0 or player["vitalidade"] > 11:
-        print("Vitalidade muito alta ou INVALIDA. Novo valor:")
-        player["vitalidade"] = int(input("Quanto de Vitalidade ? \n"))
+    player["defesa"] = int(input("Quanto de Defesa ? \n"))
+    while player["defesa"] <= 0 or player["defesa"] > 11:
+        print("Defesa muito alta ou INVALIDA. Novo valor:")
+        player["defesa"] = int(input("Quanto de Defesa ? \n"))
     player["fuga"] = int(input("Quanto de Fuga ? \n"))
     while player["fuga"] < 0 or player["fuga"] > 3:
         print("Fuga muito alta ou INVALIDA. Novo valor:")
         player["fuga"] = int(input("Quanto de Fuga ? \n"))
-def quantidadeDeEscolhas(sala):
+def quantidadeDeEscolhas(player, sala):
     quantEscolhas = random.randint(1,4)
     if sala == 0:
         print("Você esta na sala principal")
     elif quantEscolhas == 1:
         lados = random.randint(1,4)
         if lados == 1:
-            print(f"Você pode apenas ir para Cima")
             qualItem = item()
             qualInimigo = inimigo()
-            escolha = input("Oque você quer fazer ?")
-            if escolha.lower() == "cima" and qualInimigo != "Nenhum":
-                print("Derrote os inimigos primeiro")
+            if qualItem != "Nenhum":
+                print(f"Item na sala: {qualItem}")
+                usar = input("Usar item: S/N")
+                if usar.lower() == "s":
+                    aplicandoItem(player, qualItem)
+            if qualInimigo != "Nenhum":
+                print(f"Monstro na sala: {qualInimigo}")
+                usar = input("Lutar contro o Monstro agora ?: S/N")
+                if usar.lower() == "s":
+                    batalha(player,qualInimigo)
         elif lados == 2:
             print(f"Você pode apenas ir para Baixo")
             item()
@@ -173,7 +245,7 @@ player1 = {
     "nome": "jogador1",
     "itens": 0,
     "forca": 0,
-    "vitalidade": 0,
+    "defesa": 0,
     "fuga": 0
 }
 addStatus(player1)
@@ -189,6 +261,6 @@ if quantPlayer == 2:            # CRIA PLAYER 2 SE A QUANTIDADE DE JOGADORES FOR
 zero = ""
 contSala = 0
 while zero != "a":
-    criarSala(contSala)
+    criarSala(player1, contSala)
     contSala += 1
     zero = input()
